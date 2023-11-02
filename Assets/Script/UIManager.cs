043c41a2dabb2f081173b3749377997708f6d4f4
Wenyuan1008 <Wenyuan.Guo@student.uts.edu.au>
2023-11-02 17:33:41 +1100

    StartPage

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -6,15 +6,25 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Button level1Button;
+    public Button level1Button;       
+    public AudioSource audioSource;   
 
     void Start()
     {
         level1Button.onClick.AddListener(LoadMainScene);
+        if (audioSource && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 
     public void LoadMainScene()
     {
+        if (audioSource && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+
         SceneManager.LoadScene("Main");
     }
 }
